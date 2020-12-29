70eb731b56c31f48bf635269a7d77bd60251f6f0
Bop <37331768+bcmmeijer@users.noreply.github.com>
2020-12-30 00:17:49 +0100

    multiple compiles before executing possible

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <unordered_map>
 #include <memory>
 #include <Windows.h>
+#include <functional>
+#include <cassert>
 
 #include "libtcc.h"
 
@@ -30,9 +32,7 @@ public:
 
     bool compile_string(const char* str) {
         tcc_set_output_type(_s, TCC_OUTPUT_MEMORY);
-        bool ret = tcc_compile_string(_s, str) != -1;
-        tcc_relocate(_s, TCC_RELOCATE_AUTO);
-        return ret;
+        return tcc_compile_string(_s, str) != -1;
     }
 
     bool compile_file(const char* file) {
@@ -58,6 +58,7 @@ public:
 
     template <typename T>
     T get_symbol(const char* symbol) {
+        tcc_relocate(_s, TCC_RELOCATE_AUTO);
         return reinterpret_cast<T>(tcc_get_symbol(_s, symbol));
     }
 
@@ -76,14 +77,14 @@ public:
     };
 
     template <typename T, typename ... Args>
-    std::tuple<bool, typename return_type<T>::type> run_string(const char* str, const char* entry, Args ... args) {
+    std::pair<bool, typename return_type<T>::type> run_string(const char* str, const char* entry, Args ... args) {
         if (!compile_string(str))
             return { false, return_type<T>::type(0) };
         return { true, _runner<T>(entry, std::forward<Args>(args)...) };
     }
 
     template <typename T, typename ... Args>
-    std::tuple<bool, typename return_type<T>::type> run_file(const char* file, const char* entry, Args ... args) {
+    std::pair<bool, typename return_type<T>::type> run_file(const char* file, const char* entry, Args ... args) {
         if (!compile_file(file))
             return { false, return_type<T>::type(0) };
         return { true, _runner<T>(entry, std::forward<Args>(args)...) };
@@ -91,36 +92,35 @@ public:
 
 private:
     template <typename T, typename ... Args>
-    typename return_type<T>::type _runner(const char* entry_name, Args ... args) {
+    typename return_type<T>::type _runner(const char* entry, Args ... args) {
         constexpr bool argc = sizeof ... (args) != 0;
-
         using ret = typename return_type<T>::type;
         using defer = std::shared_ptr<void>;
 
         defer _(nullptr, [this](...) { this->reinit(); });
 
-        auto entry = get_symbol<T(*)(Args...)>(entry_name);
-        if (!entry) return (ret)0;
+        auto e = get_symbol<T(*)(Args...)>(entry);
+        if (!e) return (ret)0;
 
-        if constexpr (std::is_same<T, void>::value) {
-            if constexpr (argc) entry(args...);
-            else entry();
+        if constexpr (std::is_same_v<T, void>) {
+            if constexpr (argc) e(args...);
+            else e();
             return (ret)0;
         }
         else {
-            T retval;
-            if constexpr (argc) 
-                retval = entry(args...);
-            else retval = entry();
-            return retval;
+            T r;
+            if constexpr (argc) r = e(args...);
+            else r = e();
+            return r;
         }
     }
 
+private:
     TCCState* _s;
 };
 
 void test_module_print() { 
-    printf("test from script to internal\n");
+    printf("test 1 2 3\n"); 
 }
 
 struct _test_module {
@@ -131,36 +131,33 @@ static void* module_require(const char* module) {
     static std::unordered_map<std::string, void*> modules = {
         { "test_module", &test_module }
     };
-
     return modules[module];
 }
 
 static void* function_resolver(const char* dll, const char* proc) {
     HMODULE mod = nullptr;
-    
     mod = GetModuleHandleA(dll);
     if (!mod) mod = LoadLibraryA(dll);
     if (!mod) return nullptr;
-
     return GetProcAddress(mod, proc);
 }
 
 int main(int argc, char** argv) {
+
     tcc runner;
 
     runner.init();
     runner.add_symbol("require", module_require);
     runner.add_symbol("resolver", function_resolver);
 
-    auto& [script_status, script_return] = runner.run_file<int>("script.c", "entry", 0, nullptr);
-    auto& [string_status, string_return] = runner.run_string<void>(R"( 
+    runner.compile_string("int add_me(int x, int y) { return x + y; }");
 
-    int printf(const char*, ...);
-    void entry(){ 
-        printf("message from string\n"); 
-    }
+    auto [success, ret] = runner.run_string<int>(R"(
+        int a(int x, int y) { 
+            return add_me(x, y); 
+        })","a", 10, 10);
 
-    )", "entry");
+    assert(ret == 20);
 
     return 0;
 }
